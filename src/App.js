43da0e05dcf2f3690a37bd96f1e54d99dcb23f83
Bobby <SRINIVAS RAO PAKKI@LAPTOP-E1EROUB6.(none)>
2023-11-20 12:03:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// import './App.css'
 import './App.css';
+import Homepage from "./components/homepage.js"
+import Home from "./components/userhome"
+import Login from "./components/userlogin"
+import Signup from "./components/usersignup"
+import Booking from "./components/userbooking"
+import Profile from "./components/userprofile"
+import Ohome from "./components/orghome"
+import Ologin from "./components/orglogin"
+import Osignup from "./components/orgsignup"
+import Onewevent from "./components/orgnewevent"
+import Obooking from "./components/orgbooking"
+import Oprofile from "./components/orgprofile"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/userlogin" element={<Login/>}/>
+          <Route path="/usersignup" element={<Signup/>}/>
+          <Route path="/userhome" element={<Home/>}/>
+          <Route path="/userbooking" element={<Booking/>}/>
+          <Route path="/userprofile" element={<Profile/>}/>
+          <Route path="/orglogin" element={<Ologin/>}/>
+          <Route path="/orgsignup" element={<Osignup/>}/>
+          <Route path="/orghome" element={<Ohome/>}/>
+          <Route path="/orgnewevent" element={<Onewevent/>}/>
+          <Route path="/orgbooking" element={<Obooking/>}/>
+          <Route path="/orgprofile" element={<Oprofile/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
